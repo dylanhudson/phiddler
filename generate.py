ba3e3888d49384023324261114437c63a0bec2fa
Dylan Hudson <dylan@Dylans-MBP.fios-router.home>
2019-03-02 10:14:05 -0500

    initial upload

---
+++ generate.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  1 19:43:28 2019
+
+@author: dylan
+"""
+
+import numpy as np
+from keras.models import model_from_yaml
+from random import randint
+
+
+with open("rawnotes.txt") as corpus_file:
+    corpus = corpus_file.read()
+    
+print("loaded a corpus of {0} chars.".format(len(corpus)))
+
+chars = sorted(list(set(corpus)))
+num_chars = len(chars)
+encoding = {c: i for i, c in enumerate(chars)}
+decoding = {i: c for i, c in enumerate(chars)}
+print("Corpus contains {0} unique chars.".format(num_chars))
+
+bar_length = 75
+corpus_length = len(corpus)
+
+with open("model.yaml") as model_file:
+    architecture = model_file.read()
+    
+model = model_from_yaml(architecture)
+model.load_weights("weights.hdf5")
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+seed = randint(0, corpus_length - bar_length)
+seed_bar = corpus[seed:seed + bar_length]
+X = np.zeros((1, bar_length, num_chars), dtype=np.bool)
+for i, c in enumerate(seed_bar):
+    X[0, i, encoding[c]] = 1
+    
+composed_tune = ""
+for i in range(400):
+    prediction = np.argmax(model.predict(X, verbose=0))
+    
+    composed_tune += decoding[prediction]
+    
+    activations = np.zeros((1, 1, num_chars), dtype=np.bool)
+    activations[0, 0, prediction] = 1
+    X = np.concatenate((X[:, 1:, :], activations), axis=1)
+    
+
+print(composed_tune) 
+    
---
+++ robotfiddler.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 23 20:57:54 2019
+
+@author: dylan
+"""
+
+import numpy as np
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Activation
+from keras.callbacks import ModelCheckpoint
+
+
+with open("rawnotes.txt") as corpus_file:
+    corpus = corpus_file.read()
+    
+print("loaded a corpus of {0} chars.".format(len(corpus)))
+
+chars = sorted(list(set(corpus)))
+num_chars = len(chars)
+encoding = {c: i for i, c in enumerate(chars)}
+decoding = {i: c for i, c in enumerate(chars)}
+print("Corpus contains {0} unique chars.".format(num_chars))
+
+
+bar_length = 75
+skip = 1
+x_data = []
+y_data = []
+for i in range (0, len(corpus) - bar_length, skip):
+    bar = corpus[i:i + bar_length]
+    next_char = corpus[i + bar_length]
+    x_data.append([encoding[char] for char in bar])
+    y_data.append(encoding[next_char])
+    
+num_bars = len(x_data)
+print("Sliced ze corpus in to {0} bars of length{1}".format(num_bars, bar_length))
+
+print("vectorizing...")
+X = np.zeros((num_bars, bar_length, num_chars), dtype=np.bool)
+y = np.zeros((num_bars, num_chars), dtype=np.bool)
+
+for i, bar in enumerate(x_data):
+    for n, encoded_char in enumerate(bar):
+        X[i, n, encoded_char] = 1
+    y[i, y_data[i]] = 1
+    
+print("y Dimension: {0} # Sentences {1} Characters {2}".format(y.shape, num_bars, len(chars)))
+print("X Dimension: {0} # Sentences {1}".format(X.shape, bar_length))
+
+print("Time to build a brain")
+
+model = Sequential()
+model.add(LSTM(256, input_shape=(bar_length, num_chars)))
+model.add(Dense(num_chars))
+model.add(Activation('softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+#saving the model to a file
+arch = model.to_yaml()
+with open('model.yaml', 'a') as model_file:
+    model_file.write(arch)
+
+
+#setting up checkpoints to be called at the end of each epoch
+file_path = "weights-{epoch:02d}-{loss:.3f}.hdf5"
+dump_weights = ModelCheckpoint(file_path, monitor="loss", verbose=1, save_best_only=True, mode="min")
+callbacks = [dump_weights]
+
+#train
+model.fit(X, y, nb_epoch=30, batch_size=128, callbacks=callbacks)
+
+
+
+
